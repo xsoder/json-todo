b74701669df69548024f39b22191278c3417f3f1
tadihailukebe <tadihailukebe@gmail.com>
2025-06-18 19:12:51 +0000

    feat: json file check

--- main.go
+++ main.go
@@ -1,28 +1,43 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"testing"
 )
 
-func checkFile() {
-
+type task struct {
+	date  string
+	tasks string
 }
-func addTask() {
 
+func checkFile() (bool, error) {
+	tasks := task{date: "17/6/2025", tasks: "First test"}
+	json_file, err := json.Marshal(tasks.tasks)
+	if err != nil {
+		return false, err
+	}
+	_ = json_file
+	fmt.Printf("json_file: %s", json_file)
+	fmt.Println(tasks.date)
+	fmt.Println(tasks.tasks)
+	return true, nil
 }
-func deleteTask() {
 
-}
-func markTask() {
-
-}
-func displayTask() {
-
-}
-func initTask() {
-
+// func markTask() {
+//
+// }
+// func displayTask() {
+//
+// }
+func testFile(t *testing.T) {
+	state, err := checkFile()
+	if state != true || err == nil {
+		t.Errorf(`Hello("") = %t, %v, want "", error`, state, err)
+	}
 }
 func main() {
+	checkFile()
 	msg := "Hello world"
 	log := 12
 	fmt.Printf("%s", msg)
